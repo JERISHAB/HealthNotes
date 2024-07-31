7a7ff1acfe2188fc12597ba1db4f6bbdd8e2f6ec
JERISHAB <jerishabmayan@gmail.com>
2024-07-31 13:22:36 +0530

    *

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8b@qtise2wn9+ez0enq_^ed=4k)7fz5&+8#kmq60g3w*3f(03l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost','*']
 
 
 # Application definition
